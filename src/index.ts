d49db86bb354daa6014694de2a85d387ac87cc92
TheLazySquid <thelazysquid2@gmail.com>
2024-06-24 10:16:29 -0400

    Fix issues enabling plugins

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { Gimloader } from "./gimloader";
 import initInstallApi from "./installApi";
+import { onGimkit } from "./util";
 
 let loader = new Gimloader();
 window.GL = loader;
 unsafeWindow.GL = loader;
+
+if(!onGimkit) {
+    loader.pluginManager.init();
+}
 
 if(location.host === "thelazysquid.github.io" && location.pathname === "/gimloader/") {
     initInstallApi(loader);
--- types/src/pluginManager/plugin.d.ts
+++ types/src/pluginManager/plugin.d.ts
@@ -1,10 +1,12 @@
+import type { Gimloader } from "$src/gimloader";
 export default class Plugin {
+    gimloader: Gimloader;
     script: string;
     enabled: boolean;
     headers: Record<string, any>;
     return: any;
     runPlugin: boolean;
-    constructor(script: string, enabled?: boolean, initial?: boolean, runPlugin?: boolean);
+    constructor(gimloader: Gimloader, script: string, enabled?: boolean, initial?: boolean, runPlugin?: boolean);
     enable(initial?: boolean): Promise<void>;
     disable(): void;
     edit(script: string, headers: Record<string, string>): void;
