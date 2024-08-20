bb6fe9316b884eebed7447d7f8e1a6d8278b8dc5
TheLazySquid <thelazysquid2@gmail.com>
2024-08-19 21:16:42 -0400

    Add MobxUtils library

---
+++ libraries/MobxUtils.js
@@ -0,0 +1,35 @@
+/**
+ * @name MobxUtils
+ * @description Some simple utilities for react injection with MobX
+ * @author TheLazySquid
+ * @version 0.1.0
+ * @downloadUrl https://raw.githubusercontent.com/TheLazySquid/Gimloader/main/libraries/MobxUtils.js
+ * @isLibrary true
+ */
+
+let observerIntercepts = [];
+
+GL.parcel.interceptRequire("MobxUtils", exports => exports && Object.keys(exports).length === 1 && exports?.observer, exports => {
+    GL.patcher.before("MobxUtils", exports, 'observer', (_, args) => {
+        // this is our only good way of telling apart functions
+        let str = args[0].toString();
+        for(let intercept of observerIntercepts) {
+            if(intercept.match(str)) {
+                intercept.callback(args[0]);
+            }
+        }
+    })
+})
+
+export function interceptObserver(id, match, callback) {
+    observerIntercepts.push({ match, callback, id });
+}
+
+export function stopIntercepts(id) {
+    observerIntercepts = observerIntercepts.filter(intercept => intercept.id !== id);
+}
+
+export function onStop() {
+    GL.parcel.stopIntercepts("MobxUtils");
+    GL.patcher.unpatchAll("MobxUtils");
+}
