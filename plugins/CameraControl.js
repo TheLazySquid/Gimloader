1498cffc33011bba2fe6d5482b44b13b988e8e35
TheLazySquid <thelazysquid2@gmail.com>
2024-09-21 22:11:11 -0400

    [CameraControl] Sync updates with framerate
    It feels so much better to use now despite being nearly the same

--- plugins/CameraControl.js
+++ plugins/CameraControl.js
@@ -2,7 +2,7 @@
  * @name CameraControl
  * @description Lets you freely move and zoom your camera
  * @author TheLazySquid & Blackhole927
- * @version 0.2.4
+ * @version 0.3.0
  * @downloadUrl https://raw.githubusercontent.com/TheLazySquid/Gimloader/main/plugins/CameraControl.js
  * @needsLib QuickSettings | https://raw.githubusercontent.com/TheLazySquid/Gimloader/refs/heads/main/libraries/QuickSettings/build/QuickSettings.js
  * @optionalLib CommandLine | https://raw.githubusercontent.com/Blackhole927/gimkitmods/main/libraries/CommandLine/CommandLine.js
@@ -25,6 +25,11 @@ let settings = GL.lib("QuickSettings")("CameraControl", [
         min: 0.05,
         max: 20,
         default: 2
+    }, {
+        type: "boolean",
+        id: "capZoomOut",
+        title: "Cap Zoom Out",
+        default: true
     }
 ]);
 
@@ -32,12 +37,40 @@ window.addEventListener("wheel", onWheel);
 
 let freecamming = false;
 let freecamPos = {x: 0, y: 0};
-let freecamMomentum = {x: 0, y: 0};
 let scrollMomentum = 0;
-let freecamInterval = null;
-let scrollInterval = null;
 
 let stopProps = [new Set(["arrowleft"]), new Set(["arrowright"]), new Set(["arrowup"]), new Set(["arrowdown"])];
+
+let updateFreecam = null;
+let updateScroll = (dt) => {
+    let camera = GL.stores?.phaser?.scene?.cameras?.cameras?.[0];
+    if(!camera) return;
+    
+    scrollMomentum *= Math.pow(.97, dt);
+    camera.zoom += scrollMomentum * dt;
+
+    if(settings.capZoomOut) {
+        if(camera.zoom <= 0.1) {
+            scrollMomentum = 0;
+        }
+    
+        camera.zoom = Math.max(0.1, camera.zoom);
+    }
+}
+
+const patchFrame = () => {
+    if(GL.net.type !== "Colyseus") return;
+    let worldManager = GL.stores.phaser.scene.worldManager
+
+    // whenever a frame passes
+    GL.patcher.after("CameraControl", worldManager, "update", (_, args) => {
+        updateFreecam?.(args[0]);
+        updateScroll(args[0]);
+    });
+}
+
+if(GL.net.type === "Colyseus") patchFrame()
+GL.addEventListener("loadEnd", patchFrame);
 
 GL.hotkeys.addConfigurable("CameraControl", "freecam", () => {
     let scene = GL.stores?.phaser?.scene;
@@ -51,7 +84,7 @@ GL.hotkeys.addConfigurable("CameraControl", "freecam", () => {
             .get(GL.stores.phaser.mainCharacter.id).body
 
         scene.cameraHelper.startFollowingObject({ object: charObj })
-        if(freecamInterval) clearInterval(freecamInterval);
+        updateFreecam = null;
 
         for(let key of stopProps) {
             GL.hotkeys.remove(key);
@@ -69,22 +102,17 @@ GL.hotkeys.addConfigurable("CameraControl", "freecam", () => {
         }
 
         // move the camera
-        freecamInterval = setInterval(() => {
-            let moveAmount = 5 / camera.zoom;
+        updateFreecam = (dt) => {
+            let moveAmount = 0.8 / camera.zoom * dt;
             let pressed = GL.hotkeys.pressedKeys;
 
-            if(pressed.has("arrowleft")) freecamMomentum.x -= moveAmount;
-            if(pressed.has("arrowright")) freecamMomentum.x += moveAmount;
-            if(pressed.has("arrowup")) freecamMomentum.y -= moveAmount;
-            if(pressed.has("arrowdown")) freecamMomentum.y += moveAmount;
-
-            freecamPos.x += freecamMomentum.x;
-            freecamPos.y += freecamMomentum.y;
-            freecamMomentum.x *= 0.75;
-            freecamMomentum.y *= 0.75;
+            if(pressed.has("arrowleft")) freecamPos.x -= moveAmount;
+            if(pressed.has("arrowright")) freecamPos.x += moveAmount;
+            if(pressed.has("arrowup")) freecamPos.y -= moveAmount;
+            if(pressed.has("arrowdown")) freecamPos.y += moveAmount;
 
             scene.cameraHelper.goTo(freecamPos);
-        }, 1000 / 30);
+        };
     }
 
     freecamming = !freecamming;
@@ -94,19 +122,6 @@ GL.hotkeys.addConfigurable("CameraControl", "freecam", () => {
     stopPropagation: false,
     defaultKeys: new Set(["shift", "f"])
 })
-
-scrollInterval = setInterval(() => {
-    let camera = GL.stores?.phaser?.scene?.cameras?.cameras?.[0];
-    if(!camera) return;
-
-    scrollMomentum *= 0.7;
-    if (camera.zoom >= 0.1) {
-        camera.zoom += scrollMomentum;
-    } else {
-        scrollMomentum = 0;
-        camera.zoom = 0.1;
-    }
-}, 1000 / 30);
 
 // optional command line integration
 let commandLine = GL.lib("CommandLine");
@@ -125,7 +140,7 @@ if(commandLine) {
 function onWheel(e) {
     if(settings.shiftToZoom && !e.shiftKey) return;
 
-    scrollMomentum += e.deltaY / 5000;
+    scrollMomentum += e.deltaY / 65000;
 }
 
 let zoomToggled = false;
@@ -157,9 +172,7 @@ export function onStop() {
     window.removeEventListener("wheel", onWheel);
     GL.hotkeys.removeConfigurable("CameraControl", "freecam");
     GL.hotkeys.removeConfigurable("CameraControl", "zoomToggle");
-
-    clearInterval(freecamInterval);
-    clearInterval(scrollInterval);
+    GL.patcher.unpatchAll("CameraControl");
 
     if(commandLine) {
         commandLine.removeCommand("setzoom");
@@ -176,7 +189,6 @@ export function onStop() {
             .get(GL.stores.phaser.mainCharacter.id).body
 
         scene.cameraHelper.startFollowingObject({ object: charObj })
-        if(freecamInterval) clearInterval(freecamInterval);
 
         for(let key of stopProps) {
             GL.hotkeys.remove(key);
