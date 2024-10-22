854bb0a6eef58f735294d72fc807c3a8170bb841
TheLazySquid <thelazysquid2@gmail.com>
2024-10-22 16:16:05 -0400

    [PerformantGims] Add setting for what to apply to

--- plugins/PerformantGims.js
+++ plugins/PerformantGims.js
@@ -1,19 +1,51 @@
 /**
  * @name PerformantGims
- * @description Replaces gims with images of them. Looks like crap, runs really fast.
+ * @description Replaces configurable gims with images of them. Looks like crap, runs really fast.
  * @author TheLazySquid
- * @version 0.1.1
+ * @version 0.2.0
  * @downloadUrl https://raw.githubusercontent.com/TheLazySquid/Gimloader/main/plugins/PerformantGims.js
+ * @needsLib QuickSettings | https://raw.githubusercontent.com/TheLazySquid/Gimloader/refs/heads/main/libraries/QuickSettings/build/QuickSettings.js
  * @reloadRequired ingame
  */
 
+let settings = GL.lib("QuickSettings")("PerformantGims", [
+    {
+        type: "heading",
+        text: "Performant Gims Settings"
+    },
+    {
+        type: "dropdown",
+        title: "Apply To (Reload to see changes)",
+        id: "applyTo",
+        options: ["Everything", "Sentries", "Others"],
+        default: "Others"
+    }
+]);
+
+let authId = GL.stores?.phaser?.mainCharacter?.id;
+GL.net.colyseus.addEventListener("AUTH_ID", (e) => {
+    authId = e.detail;
+});
+
+function shouldApply(character) {
+    if(settings.applyTo === "Everything") return true;
+    else if(settings.applyTo === "Sentries") return character.type === "sentry";
+    console.log(character.id, authId);
+    return character.id !== authId;
+}
+
 GL.parcel.interceptRequire("PerformantGims", exports => exports?.default?.toString?.().includes('this,"applyEditStyles"'), exports => {
+    let normalSkin = exports.default;
+
     delete exports.default;
     exports.default = class {
         skinId = "character_default_cyan"
         latestSkinId = "character_default_cyan"
 
         constructor(props) {
+            if(!shouldApply(props.character)) {
+                return new normalSkin(props);
+            }
             this.character = props.character;
             this.scene = props.scene;
         }
@@ -47,8 +79,15 @@ GL.parcel.interceptRequire("PerformantGims", exports => exports?.default?.toStri
 })
 
 GL.parcel.interceptRequire("PerformantGims", exports => exports?.ANIMATION_TRACKS?.BODY, exports => {
+    let normalAnimation = exports.default;
+
     delete exports.default;
     exports.default = class {
+        constructor(props) {
+            if(!shouldApply(props.character)) {
+                return new normalAnimation(props);
+            }
+        }
         destroy() {}
         update() {}
         onSkinChanged() {}
@@ -58,3 +97,5 @@ GL.parcel.interceptRequire("PerformantGims", exports => exports?.ANIMATION_TRACK
 export function onStop() {
     GL.patcher.unpatchAll("PerformantGims");
 }
+
+export const openSettingsMenu = settings.openSettingsMenu;
