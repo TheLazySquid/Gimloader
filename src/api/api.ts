0f3e4485f1c608d0885318909474ea9b42992ab8
TheLazySquid <thelazysquid2@gmail.com>
2025-01-05 16:26:24 -0500

    Implement net, parcel and patcher

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,6 +3,7 @@ import { ParcelApi, ScopedParcelApi } from "./parcel";
 import NetApi, { type NetType } from "./net";
 import { UIApi, ScopedUIApi } from "./ui";
 import { StorageApi, ScopedStorageApi } from "./storage";
+import { PatcherApi, ScopedPatcherApi } from "./patcher";
 
 class Api {
     /** Functions used to modify Gimkit's internal modules */
@@ -23,31 +24,40 @@ class Api {
     /** Functions for persisting data between reloads */
     static storage = Object.freeze(new StorageApi());
 
+    /** Functions for intercepting the arguments and return values of functions */
+    static patcher = Object.freeze(new PatcherApi());
+
     constructor() {
-        this.parcel = new ScopedParcelApi();
-        this.hotkeys = new ScopedHotkeysApi();
-        this.net = new NetApi() as NetType;
-        this.UI = new ScopedUIApi();
-        this.storage = new ScopedStorageApi();
+        const id = "id";
+
+        this.parcel = Object.freeze(new ScopedParcelApi(id));
+        this.hotkeys = Object.freeze(new ScopedHotkeysApi());
+        this.net = Object.freeze(new NetApi() as NetType);
+        this.UI = Object.freeze(new ScopedUIApi());
+        this.storage = Object.freeze(new ScopedStorageApi());
+        this.patcher = Object.freeze(new ScopedPatcherApi(id));
     }
 
     /** Functions used to modify Gimkit's internal modules */
-    parcel: ScopedParcelApi;
+    parcel: Readonly<ScopedParcelApi>;
 
     /** Functions to listen for key combinations */
-    hotkeys: ScopedHotkeysApi;
+    hotkeys: Readonly<ScopedHotkeysApi>;
 
     /**
      * Ways to interact with the current connection to the server,
      * and functions to send general requests
      */
-    net: NetType;
+    net: Readonly<NetType>;
 
     /** Functions for interacting with the DOM */
-    UI: ScopedUIApi;
+    UI: Readonly<ScopedUIApi>;
 
     /** Functions for persisting data between reloads */
-    storage: ScopedStorageApi;
+    storage: Readonly<ScopedStorageApi>;
+
+    /** Functions for intercepting the arguments and return values of functions */
+    patcher: Readonly<ScopedPatcherApi>
 }
 
 Object.freeze(Api);
--- src/api/parcel.ts
+++ src/api/parcel.ts
@@ -1,41 +1,60 @@
+import type { Matcher } from "$src/core/parcel";
+import Parcel from "$src/core/parcel";
+import { validate } from "$src/utils";
+
 class BaseParcelApi {
     /**
      * Gets a module based on a filter, returns null if none are found
      * Be cautious when using this- plugins will often run before any modules load in,
      * meaning that if this is run on startup it will likely return nothing.
-     * Consider using {@link getLazy} instead.
+     * Consider using getLazy instead.
      */
-    query(matcher: (exports: any) => boolean) {
+    query(matcher: Matcher): any {
+        if(!validate("parcel.query", arguments, ['matcher', 'function'])) return;
 
+        return Parcel.query(matcher, false);
     }
     
     /**
      * Returns an array of all loaded modules matching a filter
      * Be cautious when using this- plugins will often run before any modules load in,
      * meaning that if this is run on startup it will likely return nothing.
-     * Consider using {@link getLazy} instead.
+     * Consider using getLazy instead.
      */
-    queryAll(matcher: (exports: any) => boolean) {
-        
+    queryAll(matcher: Matcher): any[] {
+        if(!validate("parcel.queryAll", arguments, ['matcher', 'function'])) return;
+
+        return Parcel.query(matcher, true);
     }
 }
 
 class ParcelApi extends BaseParcelApi {
     /** Waits for a module to be loaded, then runs a callback */
-    getLazy(id: string, matcher: (exports: any) => boolean, callback: (exports: any) => any) {
-
+    getLazy(id: string, matcher: Matcher, callback: (exports: any) => any, initial = true) {
+        if(!validate("parcel.getLazy", arguments,
+            ['id', 'string'], ['matcher', 'function'],
+            ['callback', 'function'], ['initial', 'boolean'])) return;
+        
+        return Parcel.getLazy(id, matcher, callback, initial);
     }
 
-    /** Cancels any calls to {@link getLazy} with the same id */
+    /** Cancels any calls to getLazy with the same id */
     stopLazy(id: string) {
+        if(!validate("parcel.stopLazy", arguments, ['id', 'string'])) return;
 
+        Parcel.stopLazy(id);
     }
 }
 
 class ScopedParcelApi extends BaseParcelApi {
+    constructor(private readonly id: string) { super() }
+    
     /** Waits for a module to be loaded, then runs a callback */
-    getLazy(matcher: (exports: any) => boolean, callback: (exports: any) => any) {
+    getLazy(matcher: Matcher, callback: (exports: any) => any, initial = true) {
+        if(!validate("parcel.getLazy", arguments,
+            ['matcher', 'function'], ['callback', 'function'], ['initial', 'boolean'])) return;
 
+        return Parcel.getLazy(this.id, matcher, callback, initial);
     }
 }
 
