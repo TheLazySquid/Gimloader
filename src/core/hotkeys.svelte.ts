275ed096b1019f75297f5a3b3b2a704520f73ac5
TheLazySquid <thelazysquid2@gmail.com>
2025-01-06 22:01:47 -0500

    Add hotkeys api

---
+++ src/core/hotkeys.svelte.ts
@@ -0,0 +1,151 @@
+import { splicer } from "$src/utils";
+
+interface HotkeyTrigger {
+    key?: string;
+    keys?: string[];
+    ctrl?: boolean;
+    shift?: boolean;
+    alt?: boolean;
+}
+
+export interface HotkeyOptions extends HotkeyTrigger {
+    preventDefault?: boolean;
+}
+
+export interface ConfigurableHotkeyOptions {
+    category: string;
+    title: string;
+    preventDefault?: boolean;
+    default?: HotkeyTrigger;
+}
+
+type Callback = (e: KeyboardEvent) => void;
+type DefaultHotkey = HotkeyOptions & { callback: Callback, id: string };
+
+export class ConfigurableHotkey {
+    id: string;
+    category: string;
+    title: string;
+    preventDefault: boolean;
+    callback: Callback;
+    trigger: HotkeyTrigger | null = $state(null);
+    default?: HotkeyTrigger;
+
+    constructor(id: string, callback: Callback, options: ConfigurableHotkeyOptions) {
+        this.id = id;
+        this.category = options.category;
+        this.title = options.title;
+        this.preventDefault = options.preventDefault ?? true;
+        this.default = options.default;
+        this.callback = callback;
+
+        if(hotkeys.savedHotkeys[id]) {
+            this.trigger = Object.assign({}, hotkeys.savedHotkeys[id]);
+        } else {
+            this.trigger = Object.assign({}, this.default);
+        }
+    }
+
+    reset() {
+        this.trigger = Object.assign({}, this.default);
+    }
+}
+
+class Hotkeys {
+    hotkeys: DefaultHotkey[] = [];
+    configurableHotkeys: ConfigurableHotkey[] = $state([]);
+    pressedKeys = new Set<string>();
+    savedHotkeys: Record<string, any> = GM_getValue('configurableHotkeys', {});
+
+    init() {
+        window.addEventListener('keydown', (event) => {
+            this.pressedKeys.add(event.code);
+            this.checkHotkeys(event);
+        });
+
+        window.addEventListener('keyup', (event) => {
+            this.pressedKeys.delete(event.code);
+        });
+
+        window.addEventListener('blur', () => {
+            this.releaseAll();
+        });
+    }
+
+    addHotkey(id: string, options: HotkeyOptions, callback: Callback) {
+        let obj = { ...options, id, callback };
+        this.hotkeys.push(obj);
+
+        return splicer(this.hotkeys, obj);
+    }
+
+    removeHotkeys(id: string) {
+        for(let i = 0; i < this.hotkeys.length; i++) {
+            if(this.hotkeys[i].id === id) {
+                this.hotkeys.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    
+    addConfigurableHotkey(id: string, options: ConfigurableHotkeyOptions, callback: Callback) {
+        let obj = new ConfigurableHotkey(id, callback, options);
+        this.configurableHotkeys.push(obj);
+
+        return splicer(this.configurableHotkeys, obj);
+    }
+
+    removeConfigurableHotkeys(id: string) {
+        for(let i = 0; i < this.configurableHotkeys.length; i++) {
+            if(this.configurableHotkeys[i].id === id) {
+                this.configurableHotkeys.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+    releaseAll() {
+        this.pressedKeys.clear();
+    }
+
+    checkHotkeys(e: KeyboardEvent) {
+        for(let hotkey of this.hotkeys) {
+            if(this.checkTrigger(e, hotkey)) {
+                if(hotkey.preventDefault || hotkey.preventDefault === undefined) e.preventDefault();
+                hotkey.callback(e);
+            }
+        }
+
+        for(let hotkey of this.configurableHotkeys) {
+            if(hotkey.trigger && this.checkTrigger(e, hotkey.trigger)) {
+                if(hotkey.preventDefault) e.preventDefault();
+                hotkey.callback(e);
+            }
+        }
+    }
+
+    checkTrigger(e: KeyboardEvent, trigger: HotkeyTrigger) {
+        if(trigger.key) {
+            if(trigger.key != e.code) return false;
+        } else {
+            if(!trigger.keys.includes(e.code)) return false;
+
+            for(let key of trigger.keys) {
+                if(!this.pressedKeys.has(key)) return false;
+            }
+        }
+
+        return (!(trigger.ctrl && !e.ctrlKey) &&
+        !(trigger.alt && !e.altKey) &&
+        !(trigger.shift && !e.shiftKey));
+    }
+
+    saveConfigurableHotkeys() {
+        this.savedHotkeys = {};
+
+        GM_setValue('configurableHotkeys', this.savedHotkeys);
+    }
+}
+
+const hotkeys = new Hotkeys();
+export default hotkeys;
--- src/utils.ts
+++ src/utils.ts
@@ -6,16 +6,34 @@ export function error(...args: any[]) {
     console.error('%c[GL]', 'color:#5030f2', ...args);
 }
 
-export const validate = (fnName: string, args: IArguments, ...schema: [string, string][]) => {
+export function validate(fnName: string, args: IArguments, ...schema: [string, string | object][]) {
     for(let i = 0; i < schema.length; i++) {
         let [ name, type ] = schema[i];
         if(args[i] === undefined) {
-            error(fnName, 'called without argument', name)
+            error(fnName, 'called without argument', name);
             return false;
         }
-        if(typeof args[i] !== type) {
-            error(fnName, 'recieved', args[i], `for argument ${name}, expected type ${type}`);
-            return false;
+        if(typeof type === "object") {
+            if(typeof args[i] !== "object") {
+                error(fnName, 'recieved', args[i], `for argument ${name}, expected type object`);
+                return false;
+            }
+
+            for(let key in type) {
+                if(args[i][key] === undefined) {
+                    error(fnName, `called without argument ${name}.${key}`);
+                    return false;
+                }
+
+                if(typeof args[i][key] !== type[key]) {
+                    error(fnName, 'recieved', args[i][key], `for argument ${name}.${key}, expected type ${type[key]}`);
+                }
+            }
+        } else {
+            if(typeof args[i] !== type) {
+                error(fnName, 'recieved', args[i], `for argument ${name}, expected type ${type}`);
+                return false;
+            }
         }
     }
 
@@ -23,3 +41,10 @@ export const validate = (fnName: string, args: IArguments, ...schema: [string, s
 }
 
 export const onGimkit = location.host === "www.gimkit.com";
+
+export function splicer(array: any[], obj: any) {
+    return () => {
+        let index = array.indexOf(obj);
+        if(index !== -1) array.splice(index, 1);
+    }
+}
