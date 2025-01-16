227bafa457fd06617173c4a0e482838b54169bd4
TheLazySquid <thelazysquid2@gmail.com>
2025-01-16 17:56:43 -0500

    Remove reroute component

--- site/src/components/Reroute.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    const searchParams = new URLSearchParams(location.search);
-    const installUrl = searchParams.get('installUrl');
-
-    if(installUrl) {
-        location.href = `/Gimloader/install${location.search}`;
-    }
-</script>
