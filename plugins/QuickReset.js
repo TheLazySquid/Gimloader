dc5ae351241a1eda819dc82c33f9657a138d1dff
TheLazySquid <thelazysquid2@gmail.com>
2025-01-22 08:53:20 -0500

    Update quickreset to use scoped api

--- plugins/QuickReset.js
+++ plugins/QuickReset.js
@@ -1,34 +1,34 @@
 /**
  * @name QuickReset
  * @description Quickly lets you restart 2d gamemodes
- * @version 0.1.0
+ * @version 0.2.0
  * @author TheLazySquid
  */
 
-GL.hotkeys.addConfigurable("QuickReset", "reset", () => {
-    if(GL.net.type !== "Colyseus") return;
-    if(!GL.stores.session.amIGameOwner) return;
+const api = new GL();
 
-    GL.net.colyseus.room.send("END_GAME");
-    GL.net.colyseus.room.send("RESTORE_MAP_EARLIER");
+api.hotkeys.addConfigurableHotkey({
+    category: "Quick Reset",
+    title: "Reset",
+    preventDefault: false,
+    default: {
+        key: "KeyR",
+        alt: true
+    }
+}, () => {
+    if(api.net.type !== "Colyseus" || !GL.net.isHost) return;
+
+    api.net.send("END_GAME");
+    api.net.send("RESTORE_MAP_EARLIER");
 
     let interval = setInterval(() => {
-        GL.net.colyseus.room.send("START_GAME");
+        api.net.send("START_GAME");
     }, 100);
 
-    let unusb = GL.net.colyseus.room.state.session.gameSession.listen("phase", (phase) => {
+    let unusb = api.net.room.state.session.gameSession.listen("phase", (phase) => {
         if(phase === "countdown") {
             clearInterval(interval);
             unusb();
         }
     });
-}, {
-    category: "Quick Reset",
-    title: "Reset",
-    preventDefault: false,
-    defaultKeys: new Set(["alt", "r"])
 });
-
-export default function onStop() {
-    GL.hotkeys.removeConfigurable("QuickReset", "reset");
-}
