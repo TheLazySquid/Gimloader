0f1ab1ca87809dd5f9165ad45a2fef336ed4ddd6
TheLazySquid <thelazysquid2@gmail.com>
2025-01-23 19:57:51 -0500

    Update idleforxp to use scoped api

--- plugins/IdleForXp.js
+++ plugins/IdleForXp.js
@@ -2,24 +2,29 @@
  * @name IdleForXp
  * @description Automatically performs actions to let you gain XP while idle
  * @author TheLazySquid
- * @version 0.1.1
- * @downloadUrl
- * @reloadRequired ingame
+ * @version 0.2.0
  * @downloadUrl https://raw.githubusercontent.com/TheLazySquid/Gimloader/main/plugins/IdleForXp.js
  */
+const api = new GL();
 
-let questions = [];
-let answerDeviceId, currentQuestionId;
+let questioner, questions;
+api.net.onLoad(() => {
+    let devices = GL.stores?.phaser.scene.worldManager.devices.allDevices;
+    questioner = devices.find(d => d.deviceOption.id == "gimkitLiveQuestion");
+    if(!questioner) return;
+
+    questions = JSON.parse(questioner.state.questions);
+});
 
 function answerQuestion() {
-    if(!currentQuestionId) return;
+    if(!questioner) return;
 
-    let question = questions.find(q => q._id == currentQuestionId);
+    let question = questions.find(q => q._id == questioner.state.currentQuestionId);
     if(!question) return;
 
     let packet = {
         key: 'answered',
-        deviceId: answerDeviceId,
+        deviceId: questioner.id,
         data: {}
     }
 
@@ -30,43 +35,13 @@ function answerQuestion() {
         packet.data.answer = correctAnswerId;
     }
 
-    GL.net.colyseus.send("MESSAGE_FOR_DEVICE", packet);
+    GL.net.send("MESSAGE_FOR_DEVICE", packet);
 };
 
-
-GL.net.colyseus.addEventListener("DEVICES_STATES_CHANGES", (event) => {
-    for(let change of event.detail.changes) {
-        let id = change[0];
-        for(let i = 0; i < change[1].length; i++) {
-            let index = change[1][i];
-            let key = event.detail.values[index];
-            let value = change[2][i];
-            if(key == "GLOBAL_questions") {
-                questions = JSON.parse(value);
-                console.log("Got questions", questions);
-                
-                answerDeviceId = id;
-            }
-        
-            let playerId = GL.stores.phaser.mainCharacter.id;
-
-            if(key == `PLAYER_${playerId}_currentQuestionId`) {
-                currentQuestionId = value;
-            }
-        } 
-    }
-});
-
-function start() {
+api.net.onLoad((type) => {
+    if(type != "Colyseus") return;
+    
     GL.notification.open({ message: "IdleForXp is active" });
-    setInterval(answerQuestion, 30000);
-}
-
-if(GL.net.type === "Colyseus") start();
-GL.addEventListener("loadEnd", () => {
-    if(GL.net.type === "Colyseus") start();
-}, { once: true })
-
-export function onStop() {
-    clearInterval(answerQuestion);
-}
+    setInterval(answerQuestion, 3000);
+    api.onStop(() => clearInterval(answerQuestion));
+});
