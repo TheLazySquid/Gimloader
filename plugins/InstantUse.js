dc424c4fe3df5833155aeb4eb30ae15ec975422a
TheLazySquid <thelazysquid2@gmail.com>
2025-01-27 11:43:31 -0500

    [InstantUse] Make hotkey configurable

--- plugins/InstantUse.js
+++ plugins/InstantUse.js
@@ -2,15 +2,19 @@
  * @name InstantUse
  * @description Instantly use nearby devices without any wait
  * @author TheLazySquid
- * @version 0.2.2
+ * @version 0.2.3
  * @webpage https://thelazysquid.github.io/Gimloader/plugins/instantuse
  * @downloadUrl https://raw.githubusercontent.com/TheLazySquid/Gimloader/main/plugins/InstantUse.js
  */
 
 const api = new GL();
 
-api.hotkeys.addHotkey({
-    key: "Enter",
+api.hotkeys.addConfigurableHotkey({
+    category: "InstantUse",
+    title: "Use Device",
+    default: {
+        key: "Enter"
+    },
     preventDefault: false
 }, () => {
     let devices = api.stores?.phaser?.scene?.worldManager?.devices;
