e8a0c0e7eff655bd0ddd87a9e2e4c66064eaa53e
TheLazySquid <thelazysquid2@gmail.com>
2025-02-06 17:33:13 -0500

    Add beginning of popup

---
+++ src/popup/Popup.svelte
@@ -0,0 +1,6 @@
+<div class="w-full h-full bg-slate-800 preflight">
+    <div class="flex items-center px-3 gap-2">
+        <img src="./images/icon128.png" alt="The Gimloader icon" class="w-6 h-6" />
+        <h1 class="text-white whitespace-nowrap text-2xl font-bold">Gimloader</h1>
+    </div>
+</div>
