1ed3fa6f507f0b926de11556bdb2e6926bc83fe9
TheLazySquid <thelazysquid2@gmail.com>
2025-02-23 11:01:21 -0500

    [QuickReset] Save start options

--- plugins/QuickReset.js
+++ plugins/QuickReset.js
@@ -1,13 +1,21 @@
 /**
  * @name QuickReset
  * @description Quickly lets you restart 2d gamemodes
- * @version 0.2.2
+ * @version 0.2.3
  * @author TheLazySquid
  * @webpage https://thelazysquid.github.io/Gimloader/plugins/quickreset
  * @downloadUrl https://raw.githubusercontent.com/TheLazySquid/Gimloader/refs/heads/main/plugins/QuickReset.js
  */
 
 const api = new GL();
+
+let startMessage = null;
+let ignoreNextStart = false;
+
+api.net.on("send:START_GAME", (message) => {
+    if(ignoreNextStart) return;
+    startMessage = message;    
+});
 
 api.hotkeys.addConfigurableHotkey({
     category: "Quick Reset",
@@ -23,14 +31,16 @@ api.hotkeys.addConfigurableHotkey({
     api.net.send("END_GAME");
     api.net.send("RESTORE_MAP_EARLIER");
 
+    ignoreNextStart = true;
     let interval = setInterval(() => {
-        api.net.send("START_GAME");
+        api.net.send("START_GAME", startMessage);
     }, 100);
 
-    let unusb = api.net.room.state.session.gameSession.listen("phase", (phase) => {
+    let unsub = api.net.room.state.session.gameSession.listen("phase", (phase) => {
         if(phase === "countdown") {
+            ignoreNextStart = false;
             clearInterval(interval);
-            unusb();
+            unsub();
         }
     });
 });
